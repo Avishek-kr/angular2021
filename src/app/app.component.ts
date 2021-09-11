ce66d373bda86700129f5ca4f342803172b2f481
Avishek-kr <ak251997@gmail.com>
2021-09-11 20:30:14 +0530

    courses angular file created

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-course';
+  data={
+    name:'Avishek'
+  }
+  onClick(){
+    alert("Hello Fuck You")
+  }
+  onKeyUp(newTitle:string){
+    this.data.name= newTitle
+  }
+}
