8959b1e515429ab63c35ff267906fd40272301a7
Avishek-kr <ak251997@gmail.com>
2021-09-12 11:45:10 +0530

    style binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -15,7 +15,7 @@ import { CoursesService } from "./courses.service";
                 {{course}}
             </li>
         </ul>
-        <button class="btn btn-primary" [class.active]="isActive  "> Save</button>
+        <button [style.backgroundColor]="isActive ? 'blue' : 'white'"> Save</button>
     `
 })
 export class CoursesComponent{
