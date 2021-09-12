7cda41017d43fddb43201d0a80a85582e0000f02
Avishek-kr <ak251997@gmail.com>
2021-09-12 11:17:04 +0530

    property binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,6 +4,7 @@ import { CoursesService } from "./courses.service";
 @Component({
     selector: 'courses',  // <courses>
     template: `<h2>{{ title }}</h2>
+    <img [src]="imageUrl" />
         <ul>
             <li *ngFor="let course of courses">
                 {{course}}
@@ -13,6 +14,7 @@ import { CoursesService } from "./courses.service";
 })
 export class CoursesComponent{
     title= "List of courses";
+    imageUrl="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"
     courses;
 
     constructor(service: CoursesService){
